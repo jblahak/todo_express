f1091409e1d639734c8388453856774944b91889
sinouJ <sinoujordan@gmail.com>
2021-03-20 00:03:02 +0100

    add header

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // Routes
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
 app.use('/api/', apiRouter)
 
 // Launch Server
