2807cfbcef8d5701c962d8471f6e998efda67eaf
sinouJ <sinoujordan@gmail.com>
2021-03-20 23:18:20 +0100

    add headers

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Headers', 'Content-Type, authorization');
     next()
 })
 app.use('/api/', apiRouter)
